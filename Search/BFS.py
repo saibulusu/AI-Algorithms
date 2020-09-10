64f4853fb6972a1cd86e54998faad3e4094affd0
saibulusu <sai.bulusu@gmail.com>
2020-09-09 19:00:42 -0700

    Fixed BFS

--- Search/BFS.py
+++ Search/BFS.py
@@ -1,27 +1,31 @@
 from Graph import Graph, Node
+from queue import Queue
 
-def BFS(graph, src, dest):
-    if src.name not in graph.nodes or dest.name not in graph.nodes:
+# Breadth First Search
+def bfs(graph, src, dest):
+    if src not in graph.nodes or dest not in graph.nodes:
         return False
-    queue = []
+    queue = Queue()
     queue.put(src)
     done = []
-    done.put(src)
+    done.append(src)
     while (not queue.empty()):
         cur = queue.get()
+        done.append(cur)
         if (cur is dest):
             return True
-        for i in cur.neighbors:
+        for i in graph.nodes[cur].neighbors:
             if i not in done:
                 queue.put(i)
     return False
 
+# Testing Code
 g = Graph()
-for i in range(ord('A'), ord('B')):
+for i in range(ord('A'), ord('K')):
     g.add_node(Node(chr(i)))
 
-edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
+edges = ['AB', 'AE', 'BF', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
 for edge in edges:
-    g.add_edge(edge[:1], edge[1:])
+   g.add_edge(edge[:1], edge[1:])
 
-print(g)
+print(bfs(g, 'A', 'C'))
--- Search/Graph.py
+++ Search/Graph.py
@@ -1,16 +1,18 @@
+# Node that contains a list of neighbor nodes
 class Node:
-	neighbors = list()
-
 	def __init__(self, name):
 		self.name = name
+		self.neighbors = list()
 	
 	def add_neighbor(self, v):
-		if v not in neighbors:
+		if v not in self.neighbors:
 			self.neighbors.append(v)
+			self.neighbors.sort()
 			return True
 		else:
 			return False
 
+# Graph which contains nodes and edges between the nodes
 class Graph:
 	nodes = {}
 
@@ -22,15 +24,15 @@ class Graph:
 			return False
 	
 	def add_edge(self, v, u):
-		if v not in self.nodes.keys or u not in self.nodes.keys:
+		if v not in self.nodes or u not in self.nodes:
 			return False
-		for i in nodes:
-			if i == v:
-				i.add_neighbor(u)
-			if i == u:
-				i.add_neighbor(v)
+		for key, value in self.nodes.items():
+			if key == v:
+				value.add_neighbor(u)
+			if key == u:
+				value.add_neighbor(v)
 		return True
 	
 	def print(self):
-		for i in self.nodes.keys():
+		for i in sorted(list(self.nodes.keys())):
 			print(i + str(self.nodes[i].neighbors))
